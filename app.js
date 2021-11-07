9749473f61c8061bd7fe6a481411c5e4c1b174d2
Alejandro De Los Santos <43819434+Pedroa03@users.noreply.github.com>
2021-11-06 23:53:06 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -14,4 +14,23 @@ app.listen(); // hace que el servidor web este a la escucha de un puerto particu
 const port = process.env.port || 3000 ; 
 app.listen(port, () => {
 
-    console.log("prueba de texto para el log de la consola"); // esta es la funcion callback que se ejecu
+    console.log("prueba de texto para el log de la consola"); // esta es la funcion callback que se ejecutara al acceder al web client. podemos verla en la consola
+    // de la terminal al ejecutar el codigo...
+});
+
+// con la conexion en
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hola Mundo');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+// });
